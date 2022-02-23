2e16a0f953b0d3e8f06a35a2be2cada0206cfaeb
nycolas mendes <nycolascouto1@gmail.com>
2022-02-23 09:47:11 -0300

    Consumo de API - Login e Cadastro

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,8 @@
+import { Postagem } from "./Postagem"
+
+export class Tema{
+
+    public id: number
+    public descricao: string
+    public postagem: Postagem[]
+}
---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,8 @@
+export class UserLogin{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public foto: string
+    public token: string
+}
