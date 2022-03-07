71c417200f777649b39d573ed5fc67144ef66422
nycolas mendes <nycolascouto1@gmail.com>
2022-03-07 14:58:41 -0300

    Atualizando metodos

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -6,7 +6,7 @@ export class Postagem{
     public id: number
     public titulo: string
     public texto: string
-    public data: Date
+    public date: Date
     public usuario: Usuario
     public tema: Tema
 }
